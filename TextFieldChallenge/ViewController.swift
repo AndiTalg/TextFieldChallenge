9566ab11a2a6164aaafbc4b23b2dd8d273b76724
AndiTalg <andreas@familie-talg.de>
2015-04-22 07:20:01 +0200

    Interface & Outlets

--- TextFieldChallenge/ViewController.swift
+++ TextFieldChallenge/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textZIP: UITextField!
+    @IBOutlet weak var textCent: UITextField!
+    @IBOutlet weak var textLock: UITextField!
+    @IBOutlet weak var switchLock: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
